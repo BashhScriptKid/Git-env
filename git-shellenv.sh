0aa3c28c7aa7a146da0f1a1d0339119493b66c19
BashhScriptKid <dev@bashh.slmail.me>
2025-12-08 20:35:53 +0800

    Update internal files to a new .local path

--- git-shellenv.sh
+++ git-shellenv.sh
@@ -34,11 +34,15 @@
 # Configuration Constants
 #------------------------------------------------------------------------------
 readonly GIT_ENV_VERSION="1.4-l"
-readonly RC_FILE="${HOME}/.git-envrc"
+
 readonly GITSH_RC_FILE="${HOME}/.gitshrc"
-readonly MAIN_HISTORY_FILE="${HOME}/.git-env_hist"
+
 readonly DEFAULT_GIT_PATH="/usr/bin/git"
+
 readonly LOCALPATH="${HOME}/.local/share/git-env/"
+readonly RC_FILE="${LOCALPATH}.git-envrc"
+readonly MAIN_HISTORY_FILE="${LOCALPATH}/.git-env_hist"
+
 readonly REMOTE_LINK="https://github.com/BashhScriptKid/Git-env"
 
 #--|CONFIG_VARS
